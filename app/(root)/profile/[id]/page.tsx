1cf17455c4c8e1079c495a6acd5dcd3f60b3faa3
jaredthecode <jaredthecode@gmail.com>
2023-09-25 18:00:49 -0400

    implement all the features

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+
 import { profileTabs } from "@/constants";
+
 import ThreadsTab from "@/components/shared/ThreadsTab";
 import ProfileHeader from "@/components/shared/ProfileHeader";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -9,7 +11,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { fetchUser } from "@/lib/actions/user.actions";
 
 async function Page({ params }: { params: { id: string } }) {
-    const user = await currentUser();
+  const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(params.id);
