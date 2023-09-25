6ce6a4e6dcc264fc6b480bcf0df0d479e460dbc6
jaredthecode <jaredthecode@gmail.com>
2023-09-25 02:05:20 -0400

    implement all the features

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,3 +1,4 @@
+import { formatDateString } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -72,12 +73,37 @@ const ThreadCard = ({
 
                       {isComment && comments.length > 0 && (
                         <Link href={`/thread/${id}`}>
-                            <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
+                            <p className="mt-1 text-subtle-medium text-gray-1">
+                                {comments.length} replies
+                                </p>
                         </Link>
                       )}  
                     </div>
                   </div>
-                </div> 
+                </div>
+
+                {/* TODO: DeleteThread */}
+                {/* TODO: Show comment logo */}
+
+               
+                {!isComment && community && (
+                    <Link href={'/communities/${community.id}'} className="mt-5 flex-items-center">
+                        <p className="text-subtle-medium text-gray-1">
+                            {formatDateString(createdAt)}
+                          {""}  - {community.name} Community
+                        </p>
+
+                        <Image 
+                           src={community.image}
+                           alt={community.name}
+                           width={14}
+                           height={14}
+                           className="ml-1 rounded-full object-cover" 
+                        />
+                    </Link>
+
+                )} 
+ 
             </div>
        </article> 
     )
